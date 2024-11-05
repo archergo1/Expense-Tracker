8613b15bf49d54348d94e16b9a1601a9de3683c4
archer <econarcher2010@gmail.com>
2024-11-05 19:16:14 +0800

    add other components

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import Header from "./components/Header";
 import Balance from "./components/Balance";
+import IncomeExpenses from "./components/IncomeExpenses";
+import TransactionList from "./components/TransactionList";
 
 import "./App.css";
 
@@ -12,6 +14,8 @@ function App() {
       <Header />
       <div className="container">
         <Balance />
+        <IncomeExpenses />
+        <TransactionList />
       </div>
     </>
   );
